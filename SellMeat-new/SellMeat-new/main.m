1a26bb82ff9d63d11ba07e44834455416e227139
cuihuajian <cuihuajian@cloudplugdeiMac.local>
2015-08-18 11:36:44 +0800

    Initial Commit

---
+++ SellMeat-new/SellMeat-new/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SellMeat-new
+//
+//  Created by cuihuajian on 15/8/18.
+//  Copyright (c) 2015年 cuihuajian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SellMeat-new/SellMeat-newTests/SellMeat_newTests.m
@@ -0,0 +1,40 @@
+//
+//  SellMeat_newTests.m
+//  SellMeat-newTests
+//
+//  Created by cuihuajian on 15/8/18.
+//  Copyright (c) 2015年 cuihuajian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SellMeat_newTests : XCTestCase
+
+@end
+
+@implementation SellMeat_newTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
